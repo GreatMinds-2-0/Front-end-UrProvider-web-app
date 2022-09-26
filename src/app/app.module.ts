aeaa5e7bcad236f3b1c2afc6a5e36202f3840950
Ryllex <epostigo1999@gmail.com>
2022-09-26 11:43:54 -0500

    feat(store):HomeStore and StoreSupplierProfile componentes created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SupplierHomeComponent } from './business/pages/supplier/supplier-home/supplier-home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { StoreHomeComponent } from './business/pages/store/store-home/store-home.component';
+import { StoreSupplierProfileComponent } from './business/pages/store/store-supplier-profile/store-supplier-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SupplierHomeComponent
+    SupplierHomeComponent,
+    StoreHomeComponent,
+    StoreSupplierProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/business/pages/store/store-supplier-profile/store-supplier-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-store-supplier-profile',
+  templateUrl: './store-supplier-profile.component.html',
+  styleUrls: ['./store-supplier-profile.component.css']
+})
+export class StoreSupplierProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
