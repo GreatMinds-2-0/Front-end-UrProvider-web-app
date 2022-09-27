cf59539f95c3fddf7ec4aa4020593ad4d157054a
Ronald-Delgado-Del-Castillo <u201810250@upc.edu.pe>
2022-09-27 01:41:01 -0500

    Add supplier-home, supplier-inventory components, servides and Update app component, db.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
+import {SupplierHomeComponent} from "./business/pages/supplier/supplier-home/supplier-home.component";
+import {SupplierInventoryComponent} from "./business/pages/supplier/supplier-inventory/supplier-inventory.component";
 
 const routes: Routes = [
-
-]
+  { path: 'supplier-home/:id', component: SupplierHomeComponent },
+  { path: 'supplier-inventory/:id', component: SupplierInventoryComponent },
+  { path: '', redirectTo: 'supplier-home/:id', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/business/pages/supplier/supplier-home/supplier-home.component.ts
+++ src/app/business/pages/supplier/supplier-home/supplier-home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Supplier} from "../../../model/supplier";
+import {SuppliersService} from "../../../services/suppliers.service";
+import {ActivatedRoute} from "@angular/router";
+import {toNumbers} from "@angular/compiler-cli/src/version_helpers";
 
 @Component({
   selector: 'app-supplier-home',
@@ -8,13 +11,22 @@ import {Supplier} from "../../../model/supplier";
 })
 export class SupplierHomeComponent implements OnInit {
 
+  id: any;
   supplierData: Supplier;
 
-  constructor() {
+  constructor(private suppliersService: SuppliersService,
+              private route: ActivatedRoute) {
     this.supplierData = {} as Supplier;
   }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get("id");
+    this.getSupplierById(Number(this.id));
   }
 
+  getSupplierById(id: number) {
+    this.suppliersService.getById(id).subscribe((response:any) => {
+      this.supplierData = response;
+    })
+  }
 }
