7d04f61faeb5aa9683b1841feacf6cda1981cf16
Rojoxt <64550279+Rojoxt@users.noreply.github.com>
2022-09-27 18:01:58 -0500

    feat: add product service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { InventoryComponent } from './inventory/pages/inventory.component';
-import { SignInComponent } from './security/pages/sign-in/sign-in.component';
-import { SignUpComponent } from './security/pages/sign-up/sign-up.component';
+import {AppRoutingModule} from './app-routing.module';
+import {InventoryComponent} from './inventory/pages/inventory.component';
+import {SignInComponent} from './security/pages/sign-in/sign-in.component';
+import {SignUpComponent} from './security/pages/sign-up/sign-up.component';
+import {StoreHomeComponent} from './store/pages/store-home/store-home.component';
+import {SupplierHomeComponent} from './supplier/pages/supplier-home/supplier-home.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     InventoryComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    StoreHomeComponent,
+    SupplierHomeComponent,
+
   ],
   imports: [
     BrowserModule,
