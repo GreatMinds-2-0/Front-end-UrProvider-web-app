2d6a3c4b61388b435b260bd91b417332cf4e438f
Rojoxt <64550279+Rojoxt@users.noreply.github.com>
2022-09-27 18:02:34 -0500

    feat: add store-home component

--- src/app/store/pages/store-home/store-home.component.spec.ts
+++ src/app/store/pages/store-home/store-home.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { StoreHomeComponent } from './store-home.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('StoreHomeComponent', () => {
+  let component: StoreHomeComponent;
+  let fixture: ComponentFixture<StoreHomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ StoreHomeComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(StoreHomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
