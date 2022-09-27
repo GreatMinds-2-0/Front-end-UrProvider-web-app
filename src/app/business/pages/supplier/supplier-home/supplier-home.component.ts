f29313359ff1a14ff54f105e11c4f661d021793d
Ronald-Delgado-Del-Castillo <u201810250@upc.edu.pe>
2022-09-27 02:35:56 -0500

    Add stores service and Update supplier-home component

--- src/app/business/pages/supplier/supplier-home/supplier-home.component.ts
+++ src/app/business/pages/supplier/supplier-home/supplier-home.component.ts
@@ -3,6 +3,8 @@ import {Supplier} from "../../../model/supplier";
 import {SuppliersService} from "../../../services/suppliers.service";
 import {ActivatedRoute} from "@angular/router";
 import {toNumbers} from "@angular/compiler-cli/src/version_helpers";
+import {StoresService} from "../../../services/stores.service";
+import {Store} from "../../../model/store";
 
 @Component({
   selector: 'app-supplier-home',
@@ -13,20 +15,30 @@ export class SupplierHomeComponent implements OnInit {
 
   id: any;
   supplierData: Supplier;
+  stores: Array<Store>;
 
   constructor(private suppliersService: SuppliersService,
+              private storesService: StoresService,
               private route: ActivatedRoute) {
     this.supplierData = {} as Supplier;
+    this.stores = [];
   }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get("id");
     this.getSupplierById(Number(this.id));
+    this.getAllStores();
   }
 
   getSupplierById(id: number) {
     this.suppliersService.getById(id).subscribe((response:any) => {
       this.supplierData = response;
+    })
+  }
+
+  getAllStores() {
+    this.storesService.getAll().subscribe((response:any) => {
+      this.stores = response;
     })
   }
 }
