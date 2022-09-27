77a1b33aaf6ae7eaec9f7f9e4635a6fe86b2ebb0
Rojoxt <64550279+Rojoxt@users.noreply.github.com>
2022-09-27 17:43:13 -0500

    feat: add security structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { InventoryComponent } from './inventory/pages/inventory.component';
+import { SignInComponent } from './security/pages/sign-in/sign-in.component';
+import { SignUpComponent } from './security/pages/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InventoryComponent
+    InventoryComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
