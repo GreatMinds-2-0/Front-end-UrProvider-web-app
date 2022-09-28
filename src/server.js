82f40877f4a76448b23c4878404eba752da48c6b
Rojoxt <64550279+Rojoxt@users.noreply.github.com>
2022-09-28 11:20:29 -0500

    chore: deploy app web in heroku

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('/dist/Front-end-UrProvider-web-app'));
+app.get('/*',(req,res)=>{
+  res.sendFile('index.html',{root:'dist/Front-end-UrProvider-web-app/'});
+});
+
+app.listen(process.env.PORT || 8080);
