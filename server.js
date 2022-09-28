d5e8a8228be1cf25307393d29d4de95074003eb7
Rojoxt <64550279+Rojoxt@users.noreply.github.com>
2022-09-28 14:17:59 -0500

    fix: fix json server deploy

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 let express = require('express');
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("server/db.json");
-const middlewares = jsonServer.defaults({ static: "./build" });
-const port = process.env.PORT || 8080;
+
 const app = express();
 
 
@@ -11,9 +7,6 @@ const app = express();
 app.use(express.static(__dirname+'/dist/urprovider-webapp'));
 
 app.get('/*',(req,resp)=> {
-  server.use(middlewares);
-  server.use(router);
-  server.listen(port);
   resp.sendFile(__dirname + '/dist/urprovider-webapp/index.html');
 });
 
