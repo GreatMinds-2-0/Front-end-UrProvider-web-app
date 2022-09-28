055c35e4b3f67f4b45f5d8aa400c79d585a4c037
Rojoxt <64550279+Rojoxt@users.noreply.github.com>
2022-09-28 13:07:03 -0500

    chore: add json server deploy

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 let express = require('express');
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("server/db.json");
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = process.env.PORT || 3000;
 
+server.use(middlewares);
+server.use(router);
+server.listen(port);
 const app = express();
 
 app.use(express.static(__dirname+'/dist/urprovider-webapp'));
