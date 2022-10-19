a8a0e55d2c9386bd2fb82207fc39d0da47d06372
Ryllex <epostigo1999@gmail.com>
2022-10-19 13:21:08 -0500

    feat: product function implemented

--- src/app/store/pages/product/product.component.ts
+++ src/app/store/pages/product/product.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Supplier} from "../../../supplier/model/supplier";
+import {Product} from "../../../inventory/model/product";
+import {SuppliersService} from "../../../supplier/services/suppliers.service";
+import {ActivatedRoute} from "@angular/router";
+import {ProductsService} from "../../../inventory/services/products.service";
+import {ProductsComponent} from "../products/products.component";
 
 @Component({
   selector: 'app-product',
@@ -6,10 +12,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  productData: Product;
+  supplierData: Supplier;
+  id: any;
+  constructor(private productsService: ProductsService,
+              private suppliersService: SuppliersService,
+              private route: ActivatedRoute) {
+    this.productData = {} as Product;
+    this.supplierData = {} as Supplier;
   }
 
+  ngOnInit(): void {
+    this.id = Number(this.route.snapshot.paramMap.get("id"));
+    this.getProductById(Number(this.id));
+    this.getSupplierById(Number(this.id));
+  }
+  getProductById(id: number) {
+    this.productsService.getById(id).subscribe((response:any) => {
+      this.productData = response;
+
+    })
+  }
+  getSupplierById(id: number) {
+    this.suppliersService.getById(id).subscribe((response:any) => {
+      this.supplierData = response;
+    })
+  }
 }
--- src/app/store/pages/products/products.component.ts
+++ src/app/store/pages/products/products.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Product} from "../../../inventory/model/product";
 import {ProductsService} from "../../../inventory/services/products.service";
 import {ActivatedRoute} from "@angular/router";
+import {Supplier} from "../../../supplier/model/supplier";
+import {SuppliersService} from "../../../supplier/services/suppliers.service";
 
 @Component({
   selector: 'app-products',
@@ -12,15 +14,19 @@ export class ProductsComponent implements OnInit {
 
   id: any;
   products: Array<Product>;
+  supplierData: Supplier;
 
   constructor(private productsService: ProductsService,
+              private suppliersService: SuppliersService,
               private route: ActivatedRoute) {
     this.products = [];
+    this.supplierData = {} as Supplier;
   }
 
   ngOnInit(): void {
     this.id = Number(this.route.snapshot.paramMap.get("id"));
     this.getAllProducts();
+    this.getSupplierById(Number(this.id));
   }
 
   getAllProducts() {
@@ -29,5 +35,11 @@ export class ProductsComponent implements OnInit {
       this.products = this.products.filter(x => x.supplierId == this.id)
     });
   }
+  getSupplierById(id: number) {
+    this.suppliersService.getById(id).subscribe((response:any) => {
+      this.supplierData = response;
+    })
+  }
+
 
 }
