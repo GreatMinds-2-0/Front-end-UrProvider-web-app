5be5db8656dbd524be2345e6ca3ee5f33acacd25
Frank_Alva <u202020026@upc.edu.pe>
2022-10-18 19:30:33 -0500

    feat: Add plans component

---
+++ src/app/supplier/pages/supplier-plan/supplier-plan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-supplier-plan',
+  templateUrl: './supplier-plan.component.html',
+  styleUrls: ['./supplier-plan.component.css']
+})
+export class SupplierPlanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
