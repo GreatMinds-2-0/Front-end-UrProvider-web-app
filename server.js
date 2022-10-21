ec09feaa382d6a8e5122334d6d548628cb9d19e5
Rojoxt <delamarzp@gmail.com>
2022-10-21 14:37:37 -0500

    chore: configuration heroku deploy

---
+++ server.js
@@ -0,0 +1,13 @@
+let express = require('express');
+
+const app = express();
+
+
+
+app.use(express.static(__dirname+'/dist/urprovider-webapp'));
+
+app.get('/*',(req,resp)=> {
+  resp.sendFile(__dirname + '/dist/urprovider-webapp/index.html');
+});
+
+app.listen(process.env.PORT || 8080);
